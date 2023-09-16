f2c860594a9f8c39dc1bc20aa12ff51773c36684
Jose Fernando Morales <contacto@jfmorales.com>
2023-09-15 21:18:49 -0500

    setup_binaries update comment with list of bins to install

--- setup_binaries.zsh
+++ setup_binaries.zsh
@@ -7,8 +7,20 @@ echo "Installing Binaries in $OS (TODO)"
 # if OS == Darwin (Mac OS)
 #   Verify if homebrew exists || install homebrew
 #   Install binaries (brew install or Brewfile)
-# elif OS == Linux (Ubuntu)
+# elif OS == Linux (Ubuntu, maybe Arch haha)
 #   Update apt and add necesary repositories
 #   Install binaries (apt install)
 
-# TODO: Veryfy if Jetbrains Mono Nerd fonts is installed || install fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
+# TODO: Veryfy if Jetbrains Mono Nerd fonts is installed || install fonts from your google drive
+#
+# TODO: binaries to install
+# - nvim
+# - lazygit
+# - bottom
+# - ripgrep
+# - bat
+# - gh (github CLI)
+# - node
+# - python
+# TODO repositories to clone
+# - astronvim: https://github.com/AstroNvim/AstroNvim
