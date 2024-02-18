346c441381765f9a6fc6e21de14b0f221774537c
Jose Fernando Morales <contacto@jfmorales.com>
2024-02-18 23:51:06 +0100

    back again harpoon

--- config/lazyvim/lua/config/keymaps.lua
+++ config/lazyvim/lua/config/keymaps.lua
@@ -38,6 +38,25 @@ end, { desc = "Term with border" })
 
 vim.keymap.set("t", "<C-l>", "clear<cr>")
 
+-- Harpoon Keymaps
+local harpoon = require("harpoon")
+
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+
+-- stylua: ignore start
+vim.keymap.set("n", "<leader>h", function () end, { desc = "Harpoon" })
+vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end, { desc = "Harpoon Append" })
+vim.keymap.set("n", "<leader>hl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon List" })
+
+vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end, { desc = "Harpoon Buffer 1" })
+vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end, { desc = "Harpoon Buffer 3" })
+vim.keymap.set("n", "<leader>h3", function() harpoon:list():select(3) end, { desc = "Harpoon Buffer 3" })
+vim.keymap.set("n", "<leader>h4", function() harpoon:list():select(4) end, { desc = "Harpoon Buffer 4" })
+vim.keymap.set("n", "<leader>h5", function() harpoon:list():select(5) end, { desc = "Harpoon Buffer 5" })
+-- stylua: ignore end
+
 -- Toggle previous & next buffers stored within Harpoon list
 -- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
 -- vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
---
+++ config/lazyvim/lua/plugins/harpoon.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "nvim-lua/plenary.nvim",
+  },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    requires = { { "nvim-lua/plenary.nvim" } },
+  },
+}
