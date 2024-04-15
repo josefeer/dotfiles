7d98572cb94f727927b8bef299a5e0d3f32c5103
Jose Fernando Morales <contacto@jfmorales.com>
2024-04-15 13:02:17 +0200

    edgy updates

--- config/lazyvim/lua/plugins/edgy.lua
+++ config/lazyvim/lua/plugins/edgy.lua
@@ -1,6 +1,10 @@
 return {
   "folke/edgy.nvim",
   event = "VeryLazy",
+  init = function()
+    vim.opt.laststatus = 3
+    vim.opt.splitkeep = "screen"
+  end,
   keys = {
     {
       "<leader>ue",
@@ -39,14 +43,6 @@ return {
             return vim.api.nvim_win_get_config(win).relative == ""
           end,
         },
-        -- {
-        --   ft = "lazyterm",
-        --   title = "LazyTerm",
-        --   size = { height = 0.4 },
-        --   filter = function(buf)
-        --     return not vim.b[buf].lazyterm_cmd
-        --   end,
-        -- },
         "Trouble",
         { ft = "qf", title = "QuickFix" },
         {
@@ -57,12 +53,10 @@ return {
             return vim.bo[buf].buftype == "help"
           end,
         },
-        -- { title = "Spectre", ft = "spectre_panel", size = { height = 0.4 } },
-        -- { title = "Neotest Output", ft = "neotest-output-panel", size = { height = 15 } },
       },
       left = {
         {
-          title = "Nvim-Tree",
+          title = "Nvim-Tree                            ",
           ft = "NvimTree",
           pinned = true,
           open = function()
@@ -70,49 +64,12 @@ return {
           end,
           size = { height = 0.65  },
         },
-        -- { title = "Neotest Summary", ft = "neotest-summary" },
-        -- {
-        --   title = "Neo-Tree Git",
-        --   ft = "neo-tree",
-        --   filter = function(buf)
-        --     return vim.b[buf].neo_tree_source == "git_status"
-        --   end,
-        --   pinned = true,
-        --   open = "Neotree position=right git_status",
-        -- },
-        -- {
-        --   title = "Neo-Tree Buffers",
-        --   ft = "neo-tree",
-        --   filter = function(buf)
-        --     return vim.b[buf].neo_tree_source == "buffers"
-        --   end,
-        --   pinned = true,
-        --   open = "Neotree position=top buffers",
-        -- },
         {
-          title = "Outline",
+          title = "Outline                              ",
           ft = "Outline",
           pinned = true,
           open = "Outline",
         },
-      },
-      keys = {
-        -- increase width
-        ["<c-Right>"] = function(win)
-          win:resize("width", 2)
-        end,
-        -- decrease width
-        ["<c-Left>"] = function(win)
-          win:resize("width", -2)
-        end,
-        -- increase height
-        ["<c-Up>"] = function(win)
-          win:resize("height", 2)
-        end,
-        -- decrease height
-        ["<c-Down>"] = function(win)
-          win:resize("height", -2)
-        end,
       },
     }
     return opts
