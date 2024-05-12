6cc363088be0b49ab98b0328d5bb47a4e076d893
Jose Fernando Morales <contacto@jfmorales.com>
2024-05-11 22:44:14 -0500

    skhdrc and keymaps update

--- config/lazyvim/lua/plugins/disabled.lua
+++ config/lazyvim/lua/plugins/disabled.lua
@@ -3,5 +3,5 @@ return {
   { "nvim-spectre", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "echasnovski/mini.ai", enabled = false },
-  { "echasnovski/mini.surround", enabled = false },
+  { "L3MON4D3/LuaSnip", enabled = false },
 }
