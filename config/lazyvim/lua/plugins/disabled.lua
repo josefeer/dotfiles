dbd6c79ba9ef785a121deb4bd3a8a437807842d6
Jose Fernando Morales <contacto@jfmorales.com>
2024-06-29 21:50:24 -0500

    kitty, lazyvim and yabai updates

--- config/lazyvim/lua/plugins/disabled.lua
+++ config/lazyvim/lua/plugins/disabled.lua
@@ -1,6 +1,5 @@
 return {
   { "nvim-spectre", enabled = false },
   { "folke/flash.nvim", enabled = false },
-  { "echasnovski/mini.ai", enabled = false },
   { "L3MON4D3/LuaSnip", enabled = false },
 }
