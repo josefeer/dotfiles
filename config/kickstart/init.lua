28d13c85d7466aa4c9fa93f03c60a4d42f4b43d5
Jose Fernando Morales <contacto@jfmorales.com>
2024-10-19 13:39:20 +0200

    personal keybinds

--- config/kickstart/init.lua
+++ config/kickstart/init.lua
@@ -190,6 +190,10 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+-- Personal Keybinds
+vim.keymap.set("n", "<C-s>", "<cmd>w<CR>", { desc = "Write File" })
+vim.keymap.set("n", "<leader>Q", "<cmd>qa<CR>", { desc = "Quit All"})
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
