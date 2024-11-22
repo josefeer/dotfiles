c9dab381551212aff4e5bbaa803e436991283276
Jose Fernando Morales <contacto@jfmorales.com>
2024-11-22 10:00:46 +0100

    nvim: replace harpoon for arrow

---
+++ config/kickstart/lua/plugins/arrow.lua
@@ -0,0 +1,23 @@
+--[[
+
+  Intuitive Marks
+
+--]]
+
+return {
+  "otavioschwanck/arrow.nvim",
+  dependencies = {
+    { "nvim-tree/nvim-web-devicons" },
+  },
+  event = "VeryLazy",
+  opts = {
+    separate_by_branch = true,
+    separate_save_and_remove = true, -- if true, will remove the toggle and create the save/remove keymaps.
+    show_icons = true,
+    leader_key = ';', -- Recommended to be a single key
+    buffer_leader_key = 'M', -- Per Buffer Mappings, dettached from regular marks
+    per_buffer_config = {
+      lines = 5,
+    }
+  }
+}
--- config/kickstart/lua/plugins/harpoon.lua
+++ config/kickstart/lua/plugins/harpoon.lua
@@ -6,6 +6,7 @@
 
 return {
   "ThePrimeagen/harpoon",
+  enabled = false,
   event = "VeryLazy",
   branch = "harpoon2",
   dependencies = { "nvim-lua/plenary.nvim" },
