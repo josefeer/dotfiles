fe85adba3d52072ea9ea87e25f843b499d65a64c
Jose Fernando Morales <contacto@jfmorales.com>
2024-12-27 22:19:14 -0500

    nvim: telescope picker for hidden files

--- .config/nvim-kickstart/lua/plugins/telescope.lua
+++ .config/nvim-kickstart/lua/plugins/telescope.lua
@@ -91,6 +91,7 @@ return {
     -- See `:help telescope.builtin`
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+    vim.keymap.set("n", "<leader>si", function() builtin.find_files({hidden=true}) end , { desc = "[S]earch H[I]dden Files" })
     vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
     vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
     vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
