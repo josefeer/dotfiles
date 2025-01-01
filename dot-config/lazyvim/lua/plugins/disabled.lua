e1b7e0626b8c34198036dba62fa8de7263c8d76c
Jose Fernando Morales <contacto@jfmorales.com>
2025-01-01 14:43:10 -0500

    lazyvim: deprecated

--- dot-config/lazyvim/lua/plugins/disabled.lua
+++
@@ -1,4 +0,0 @@
-return {
-  { "folke/flash.nvim", enabled = false },
-  { "L3MON4D3/LuaSnip", enabled = false },
-}
--- dot-config/lazyvim/lua/plugins/trouble.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "folke/trouble.nvim",
-  opts = {
-    win_config = { border = "rounded" },
-  },
-}
