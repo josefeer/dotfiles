e6187ae8a791bded61981c3754aa970422fe9e89
Jose Fernando Morales <contacto@jfmorales.com>
2025-10-18 17:39:42 -0500

    nvim: keybinds

--- dot-config/jfmdnvim/lua/plugins/snacks.lua
+++ dot-config/jfmdnvim/lua/plugins/snacks.lua
@@ -16,28 +16,16 @@ return {
     image = {},
   },
   keys = {
+    { "<leader>sf", function() Snacks.picker.files({ignored = true, hidden = true}) end, desc = "[F]iles" },
+    { "<leader>s,", function() Snacks.picker.files({ cwd = vim.fn.stdpath("data") .. "/scratch" }) end, desc = "Scratches [,]" },
+    { "<leader>s.", function() Snacks.picker.recent() end, desc = "Recent [.]" },
+    { "<leader>s/", function() Snacks.picker.buffers() end, desc = "Buffers [/]" },
     { "<leader>sh", function() Snacks.picker.help() end, desc = "[H]elp Pages" },
-    { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "[K]eymaps" },
-    { "<leader>sf", function() Snacks.picker.files() end, desc = "[F]iles" },
     { "<leader>sw", function() Snacks.picker.grep_word() end, desc = "Visual selection or word", mode = { "n", "x" } },
     { "<leader>sg", function() Snacks.picker.grep() end, desc = "[G]rep" },
-    { "<leader>sd", function() Snacks.picker.diagnostics() end, desc = "[D]iagnostics" },
     { "<leader>sr", function() Snacks.picker.resume() end, desc = "[R]esume" },
-    { "<leader>s.", function() Snacks.picker.recent() end, desc = "Recent [.]" },
-    { "<leader>sb", function() Snacks.picker.buffers() end, desc = "[B]uffers" },
-    { "<leader>st", function() Snacks.picker.git_status() end, desc = "Git S[T]atus" },
-    { "<leader>ss", function() Snacks.picker.files({ cwd = vim.fn.stdpath("data") .. "/scratch" }) end, desc = "[S]cratches" },
-    --
-    -- -- It's also possible to pass additional configuration options.
-    -- --  See `:help telescope.builtin.live_grep()` for information about particular keys
-    -- vim.keymap.set("n", "<leader>s/", function()
-    --   builtin.live_grep({
-    --     grep_open_files = true,
-    --     prompt_title = "Live Grep in Open Files",
-    --   })
-    -- end, { desc = "[S]earch [/] in Open Files" })
-    { "<leader>s/", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers [/]" },
-    --
+    { "<leader>st", function() Snacks.picker.git_status() end, desc = "Git Status [t]" },
+    { "<leader>sb", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers [b]" },
     -- -- Shortcut for searching your Neovim configuration files
     -- vim.keymap.set("n", "<leader>sn", function()
     --   builtin.find_files({ cwd = vim.fn.stdpath("config") })
