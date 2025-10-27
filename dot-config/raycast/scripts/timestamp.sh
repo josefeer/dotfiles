bd1d601b00e90473ac1ef2c163c180fdb74f896d
Jose Fernando Morales <contacto@jfmorales.com>
2025-10-26 23:08:55 -0500

    raycast

---
+++ dot-config/raycast/scripts/timestamp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Timestamp
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author jose_morales_de_leon
+# @raycast.authorURL https://raycast.com/jose_morales_de_leon
+
+date | pbcopy
+echo "Timestamp in Clipboard"
+
