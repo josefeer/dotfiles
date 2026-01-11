700f492867055e708e318bd80bcd02db35d239e9
Jose Fernando Morales <contacto@jfmorales.com>
2026-01-11 12:14:28 -0500

    nvim/lsp: comment out what we barely understand

--- dot-config/jfmdnvim/lua/plugins/lsp.lua
+++ dot-config/jfmdnvim/lua/plugins/lsp.lua
@@ -189,23 +189,23 @@ return {
 
         ts_ls = {},
         pyright = {},
-        html = {},
-        cssls = {},
+        -- html = {},
+        -- cssls = {},
 
-        lua_ls = {
+        -- lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
           -- capabilities = {},
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = "Replace",
-              },
+          -- settings = {
+          --   Lua = {
+          --     completion = {
+          --       callSnippet = "Replace",
+          --     },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
               -- diagnostics = { disable = { 'missing-fields' } },
-            },
-          },
-        },
+        --     },
+        --   },
+        -- },
       }
 
       -- Ensure the servers and tools above are installed
